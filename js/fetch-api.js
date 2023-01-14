3582611a6b44e237c2edb58ca62d2f1e3880138c
Matt <sparklingtoast98@gmail.com>
2023-01-14 14:59:20 -0600

    added js dir

---
+++ js/fetch-api.js
@@ -0,0 +1,52 @@
+// 293. Using the Fetch API
+
+// One request:
+// fetch("https://swapi.dev/api/people/1/")
+//     .then(res => {
+//         console.log("RESOLVED!", res);
+//         return res.json(); // not the same as JSON.parse - this returns a promise
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(e => {
+//         console.log("ERROR!", e);
+//     });
+
+// Chaining requests:
+// fetch("https://swapi.dev/api/people/1/")
+//     .then(res => {
+//         console.log("First result resolved:", res);
+//         return res.json(); // not the same as JSON.parse - this returns a promise
+//     })
+//     .then(data => {
+//         console.log(data);
+//         return fetch("https://swapi.dev/api/people/2");
+//     })
+//     .then(res => {
+//         console.log("Second result resolved:");
+//         return res.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(e => {
+//         console.log("ERROR!", e);
+//     })
+
+// ASYNC function:
+const loadStarWarsPeople = async () => {
+    try {
+        const res = await fetch("https://swapi.dev/api/people/1/");
+        const data = await res.json();
+        console.log(data);
+
+        const res2 = await fetch("https://swapi.dev/api/people/2/");
+        const data2 = await res2.json();
+        console.log(data2);
+    } catch (e) {
+        console.log("ERROR!", e);
+    }
+}
+
+loadStarWarsPeople();
---
+++ js/xhr.js
@@ -0,0 +1,18 @@
+// 292. Making XHRs
+
+const req = new XMLHttpRequest();
+
+req.onload = function () {
+    console.log("IT LOADED!!!");
+    console.log(this);
+    const data = JSON.parse(this.responseText);
+    console.log(data.name, data.height);
+};
+
+req.onerror = function () {
+    console.log("ERROR!!!");
+    console.log(this);
+};
+
+req.open("GET", "https://swapi.dev/api/people/1/");
+req.send();
