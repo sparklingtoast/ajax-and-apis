8dc9636ad4d1a537f364ee34436660706ef2acbd
Matt <sparklingtoast98@gmail.com>
2023-01-14 15:48:39 -0600

    axios example using header for json content type

--- js/axios.js
+++ js/axios.js
@@ -19,5 +19,30 @@ const getStarWarsPerson = async (id) => {
     }
 };
 
-getStarWarsPerson(5);
-getStarWarsPerson(10);
+// getStarWarsPerson(5);
+// getStarWarsPerson(10);
+
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('button');
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLI = document.createElement('LI');
+
+    newLI.append(jokeText);
+    jokes.append(newLI);
+};
+
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: 'application/json' } };
+        const res = await axios.get('https://icanhazdadjoke.com/', config);
+
+        console.log(res);
+        return res.data.joke;
+    } catch (e) {
+        return "No jokes available. Sorry!";
+    }
+};
+
+button.addEventListener('click', addNewJoke);
